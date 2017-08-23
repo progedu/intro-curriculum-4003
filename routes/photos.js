bc86144368ee1052725d92ecfca18d1ee825608c
yusuke_nakajima <www.yyworld@gmail.com>
2017-08-23 12:41:23 +0900

    コード整形

--- routes/photos.js
+++ routes/photos.js
@@ -3,16 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('Some photos');
+    res.send('Some photos');
 });
 
 router.param('title', (req, res, next, title) => {
-  res.send(title);
-  next();
+    res.send(title);
+    next();
 });
 
 router.get('/:title', (req, res, next) => {
-  res.end();
+    res.end();
 });
 
 
