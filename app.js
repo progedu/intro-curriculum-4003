6264763fdd89715105aaf577f74e883c8d1e4acf
Iwasaki <iwasaki@arssystem.co.jp>
2018-04-26 07:27:10 +0000

    usersを認証時のみ閲覧可能

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ passport.use(new GitHubStrategy({
       return done(null, profile);
     });
   }
-  ));
+));
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -56,7 +56,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/users', ensureAuthenticated, users);
 app.use('/photos', photos);
 
 app.get('/auth/github',
@@ -79,15 +79,20 @@ app.get('/logout', function (req, res) {
   res.redirect('/');
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
