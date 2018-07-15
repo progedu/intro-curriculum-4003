9f0af852088f24ccbf5696e0414ddf053aa36c4d
mbyk <marubayashi@gmail.com>
2018-07-15 02:18:47 +0000

    authenticate users path

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var session = require('express-session');
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
 
-var GITHUB_CLIENT_ID = 'f756acb8748f85e2014b';
-var GITHUB_CLIENT_SECRET = '0fc57f6660bd5da78873eeacda8c131859b64f30';
+var GITHUB_CLIENT_ID = 'fill out client id';
+var GITHUB_CLIENT_SECRET = 'fill out client secret';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -56,7 +56,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/users', ensureAuthenticated ,users);
 app.use('/photos', photos);
 
 app.get('/auth/github',
@@ -96,5 +96,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 
 module.exports = app;
