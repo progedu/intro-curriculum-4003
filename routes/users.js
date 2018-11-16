bf19719f9c8aaee1aec21bdb972f0a629a1dc758
heha-proglea <heha965+proglea@gmail.com>
2018-11-16 13:16:45 +0000

    Update: ES5 => ES6

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
