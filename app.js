f55c687b7e7db7945542add3deb358f741ef79e8
nonoike <ij4nu8d4fw@gmail.com>
2018-12-23 18:37:54 +0900

    /users は認証が完了していないと見れないように

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ passport.use(new GitHubStrategy({
       return done(null, profile);
     });
   }
-  ));
+));
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -56,7 +61,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/users', ensureAuthenticated, users);
 app.use('/photos', photos);
 
 app.get('/auth/github',
@@ -80,14 +85,14 @@ app.get('/logout', function (req, res) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
