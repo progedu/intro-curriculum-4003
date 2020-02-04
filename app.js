47c58e3c932155336ecc53fd2f4028a442c137ea
Kazuyuki Matsuda <mtdkzyk.github@gmail.com>
2020-02-04 14:15:12 +0900

    unchange ID

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var session = require('express-session');
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
 
-var GITHUB_CLIENT_ID = '594615d612367a34685a';
-var GITHUB_CLIENT_SECRET = '35b5f1d3dc40fe83f7831e91ad735491c0db03b8';
+var GITHUB_CLIENT_ID = 'f756acb8748f85e2014b';
+var GITHUB_CLIENT_SECRET = '0fc57f6660bd5da78873eeacda8c131859b64f30';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -54,7 +54,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: '2b62f8cbd95bf35d', resave: false, saveUninitialized: false }));
+app.use(session({ secret: '417cce55dcfcfaeb', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
