522dcda1a6382d19f5c991939d12a855acec8fde
seipan1213 <seiju1213@gmail.com>
2020-03-16 00:27:36 +0900

    usersアクセスのための認証

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ passport.use(new GitHubStrategy({
   }
 ));
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var photosRouter = require('./routes/photos');
@@ -53,8 +55,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/photos', photosRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
 
 app.get('/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
@@ -75,6 +77,11 @@ app.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
