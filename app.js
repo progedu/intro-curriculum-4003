7edd8d2888f6fd911dbb2eed1388ef70583658aa
anazsama <anazsama@outlook.jp>
2020-05-19 08:00:58 +0900

    /usersにアクセスするにはGitHub認証を前提とするための修正

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var session = require('express-session');
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
 
-var GITHUB_CLIENT_ID = 'f756acb8748f85e2014b';
-var GITHUB_CLIENT_SECRET = '0fc57f6660bd5da78873eeacda8c131859b64f30';
+var GITHUB_CLIENT_ID = '3fff90786e5034843b88';
+var GITHUB_CLIENT_SECRET = 'b38b7104431b91df036830fd3c430780f1186d54';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -53,7 +53,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
@@ -93,3 +93,8 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
+
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){ return next();}
+  res.redirect('/login');
+}
