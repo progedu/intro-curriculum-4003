1fbde60f0d0051ce1a480ff2a1a6d2cf57d07e04
KevinST <isono1009@yahoo.co.jp>
2021-01-04 23:52:59 +0900

    /usersへのアクセスに認証を追加

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ passport.use(new GitHubStrategy({
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var photosRouter = require('./routes/photos');
+const { Router } = require('express');
 
 var app = express();
 app.use(helmet());
@@ -53,7 +54,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', eusureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
@@ -75,6 +76,13 @@ app.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
+
+function eusureAuthenticated(req, res, next){
+  if (req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('./login');
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
