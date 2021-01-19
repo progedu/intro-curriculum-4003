3c100ef7ed890af3b50ddf577bd9ba1728b694c4
rascasse-monaco <mktwkhr@gmail.com>
2021-01-19 21:34:33 +0900

    環境変数から読み出し

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var session = require('express-session');
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
-
-var GITHUB_CLIENT_ID = 'f756acb8748f85e2014b';
-var GITHUB_CLIENT_SECRET = '0fc57f6660bd5da78873eeacda8c131859b64f30';
+const dotenv = require('dotenv').config();//環境変数を.envファイルから読みだすモジュールを呼び出し
+const env = process.env;
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -20,8 +20,8 @@ passport.deserializeUser(function (obj, done) {
 });
 
 passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
+  clientID: env.CLIENT_ID,//環境変数から呼び出し
+  clientSecret: env.CLIENT_SECRET,//環境変数から呼び出し
   callbackURL: 'http://localhost:8000/auth/github/callback'
 },
   function (accessToken, refreshToken, profile, done) {
@@ -29,32 +29,34 @@ passport.use(new GitHubStrategy({
       return done(null, profile);
     });
   }
-));
+  ));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var photosRouter = require('./routes/photos');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var photos = require('./routes/photos');
 
 var app = express();
 app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: '417cce55dcfcfaeb', resave: false, saveUninitialized: false }));
+app.use(session({ secret: env.SESSION_SECRET, resave: false, saveUninitialized: false }));//環境変数から呼び出し
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/photos', photosRouter);
+app.use('/', routes);
+app.use('/users', users);
+app.use('/photos', photos);
 
 app.get('/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
@@ -68,7 +70,7 @@ app.get('/auth/github/callback',
   });
 
 app.get('/login', function (req, res) {
-  res.render('login');
+  res.render('login', { user: req.user });
 });
 
 app.get('/logout', function (req, res) {
@@ -77,19 +79,35 @@ app.get('/logout', function (req, res) {
 });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
