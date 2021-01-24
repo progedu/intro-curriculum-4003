4832eb5f431ba68e46316afdd44d4a50151eac84
伊藤弓次郎 <kyujiro.ito@gmail.com>
2021-01-24 21:45:55 +0900

    練習問題

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ passport.use(new GitHubStrategy({
   }
 ));
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var photosRouter = require('./routes/photos');
@@ -53,7 +60,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
