9ae944d4d5fbf135113d9b81c60049e873226316
Atsunori Hattori <tihove7k@gmail.com>
2021-03-26 02:03:54 +0900

    /usersは認証しないと見れないようにする

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var session = require('express-session');
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
 
-var GITHUB_CLIENT_ID = 'f756acb8748f85e2014b';
-var GITHUB_CLIENT_SECRET = '0fc57f6660bd5da78873eeacda8c131859b64f30';
+var GITHUB_CLIENT_ID = 'b38e649e0dc05e5605be';
+var GITHUB_CLIENT_SECRET = '51d800e15552387c0b2fc18daa208ec90214a644';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -53,7 +53,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 app.get('/auth/github',
@@ -75,6 +75,13 @@ app.get('/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
